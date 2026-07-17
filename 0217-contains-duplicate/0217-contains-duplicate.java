8e069eb74c0976e0267834b27b52a72dcd0bc3bc
gin7toki <73631567+DeveshRohit@users.noreply.github.com>
2026-07-17 12:28:22 +0530

    Delete 0217-contains-duplicate/0217-contains-duplicate.java

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++
@@ -1,8 +0,0 @@
-import java.util.HashSet;
-import java.util.stream.Stream;
-
-class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        return Arrays.stream(nums).distinct().count() < nums.length;
-    }
-}
