0c4abc810357f1bcb345cc5572a6951ecd775b70
gin7toki <73631567+DeveshRohit@users.noreply.github.com>
2026-08-30 00:45:24 +0530

    Time: 47 ms (29.35%), Space: 19.6 MB (18.53%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,12 +1,26 @@
 from collections import Counter
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        left = 0
+        if len(s1) > len(s2):
+            return False
+
         freq = Counter(s1)
-        for right in range(len(s1)-1, len(s2)):
-            if(Counter(s2[left:right+1]) == freq):
+        window_freq = Counter(s2[:len(s1)]) #pre fill with first window
+
+        if freq == window_freq:
+            return True
+
+        for i in range(len(s1), len(s2)):
+            window_freq[s2[i]] += 1
+
+            left_char = s2[i - len(s1)]
+            window_freq[left_char] -= 1
+
+            # deleting key if it hits 0 so that '==' check works
+            if window_freq[left_char] == 0:
+                del window_freq[left_char]
+
+            if freq == window_freq:
                 return True
-            else:
-                left+=1
-                right+=1
+
         return False
