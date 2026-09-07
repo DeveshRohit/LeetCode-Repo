ad91bccc08f2016ba5db0a9c082565ec0bc9a003
gin7toki <73631567+DeveshRohit@users.noreply.github.com>
2026-09-08 00:17:34 +0530

    Time: 166 ms (66.47%), Space: 20.6 MB (81.94%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -7,8 +7,8 @@ class Solution:
             total = 0
             k = (left+right) // 2
             for pile in piles:
-                total += math.ceil(pile/k)
-            if total <= h and k < minm:
+                total += (pile + k - 1) // k
+            if total <= h:
                 minm = k
                 right = k - 1
             else:
